158704225f5560e03a174e825b841eae539af913
Cassandra Caestecker <caesteckercassandra@gmail.com>
2019-05-16 16:56:52 +0200

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -19,13 +19,13 @@
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
-define('DB_NAME', 'fooDog');
+define('DB_NAME', 'kqsdnqs;fhsk');
 
 /** Utilisateur de la base de données MySQL. */
-define('DB_USER', 'root');
+define('DB_USER', 'fksjfhskuf');
 
 /** Mot de passe de la base de données MySQL. */
-define('DB_PASSWORD', 'user');
+define('DB_PASSWORD', 'ndqsjkdnqkdq');
 
 /** Adresse de l’hébergement MySQL. */
 define('DB_HOST', 'localhost');
