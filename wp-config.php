1130226c3829e3342603f293d13c5ca7c81a10b4
Cassandra Caestecker <caesteckercassandra@gmail.com>
2019-05-17 09:46:56 +0200

    plugins ajoutés - backup

--- wp-config.php
+++ wp-config.php
@@ -19,13 +19,13 @@
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
-define('DB_NAME', 'kqsdnqs;fhsk');
+define('DB_NAME', 'fooDog');
 
 /** Utilisateur de la base de données MySQL. */
-define('DB_USER', 'fksjfhskuf');
+define('DB_USER', 'root');
 
 /** Mot de passe de la base de données MySQL. */
-define('DB_PASSWORD', 'ndqsjkdnqkdq');
+define('DB_PASSWORD', 'user');
 
 /** Adresse de l’hébergement MySQL. */
 define('DB_HOST', 'localhost');
