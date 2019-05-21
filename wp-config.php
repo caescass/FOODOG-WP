e7a6578eb028b56d2e52eaffa2c31286947e3254
Oliver-Vandevelde <oliver.vandevelde7@gmail.com>
2019-05-21 15:46:05 +0200

    articleMaj

--- wp-config.php
+++ wp-config.php
@@ -19,13 +19,13 @@
 
 // ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
 /** Nom de la base de données de WordPress. */
-define('DB_NAME', 'fooDog');
+define('DB_NAME', 'test');
 
 /** Utilisateur de la base de données MySQL. */
-define('DB_USER', 'root');
+define('DB_USER', 'user');
 
 /** Mot de passe de la base de données MySQL. */
-define('DB_PASSWORD', 'Soufianel');
+define('DB_PASSWORD', 'password');
 
 /** Adresse de l’hébergement MySQL. */
 define('DB_HOST', 'localhost');
